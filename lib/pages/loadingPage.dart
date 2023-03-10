a24b6a5d03671c2e5a19f84fe1c69417db5450d2
Bahaa Zuraik <bahaa_zz@hotmail.com>
2023-03-09 21:09:47 -0500

    added few pages with navigation bar

---
+++ lib/pages/loadingPage.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'homePage.dart';
+
+class LoadingPage extends StatefulWidget {
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 10), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SvgPicture.asset(
+          'assets/images/logo.svg',
+          width: 200,
+          height: 200,
+        ),
+      ),
+    );
+  }
+}
